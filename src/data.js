c762d31ac94283703e41f5e7b4c5d37e7b787c52
Kapil0187 <kapilbarsker2003@gmail.com>
2023-12-06 19:44:07 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,40 @@
+
+const reviews = [
+    {
+        id : 1,
+        name : "Pranay Gupta",
+        job : "Software Engineer",
+        image : "https://t3.ftcdn.net/jpg/03/07/57/54/360_F_307575473_NaZ8XNxe1BBt5Z0fKgMZWJgb1JIzDuYR.webp",
+        text : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae massa erat. Aenean felis orci, faucibus sed rutrum in, efficitur vitae mauris. Aenean euismod rhoncus neque sed mollis."
+    },
+    {
+        id : 2,
+        name : "Kapil Barsker",
+        job : "Fronted Developer",
+        image : "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=600",
+        text : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae massa erat. Aenean felis orci, faucibus sed rutrum in, efficitur vitae mauris. Aenean euismod rhoncus neque sed mollis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. "
+    },
+    {
+        id : 3,
+        name : "Hardik Kushwaha",
+        job : "Backed Developer",
+        image : "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600",
+        text : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae massa erat. Aenean felis orci, faucibus sed rutrum in, efficitur vitae mauris. Aenean euismod rhoncus neque sed mollis.  "
+    },
+    {
+        id : 4,
+        name : "Mohit Malviya",
+        job : "Software Engineer",
+        image : "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=600",
+        text : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae massa erat. Aenean felis orci, faucibus sed rutrum in, efficitur vitae mauris. Aenean euismod rhoncus neque sed mollis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. "
+    },
+    {
+        id : 5,
+        name : "Harshit Dongere",
+        job : "Junior Software Engineer",
+        image : "https://images.pexels.com/photos/1516680/pexels-photo-1516680.jpeg?auto=compress&cs=tinysrgb&w=600",
+        text : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae massa erat. Aenean felis orci, faucibus sed rutrum in, efficitur vitae mauris. Aenean euismod rhoncus neque sed mollis. Class aptent taciti sociosqu ad litora torquent per. "
+    }
+]
+
+export default reviews;
